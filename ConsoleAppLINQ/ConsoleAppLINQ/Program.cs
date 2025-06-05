f2d1ab9df33a97cc155a7c224bdcad917bcce712
Tatiana <korbunt@gmail.com>
2025-06-05 12:48:20 +0900

    Test3

---
+++ ConsoleAppLINQ/ConsoleAppLINQ/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace hehe12;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 0;
+        int b = 0;
+        int result = 0;
+
+        while (true)
+        {
+            try
+            {
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+                result = a / b;
+                Console.WriteLine(result);
+
+                break;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("wrong input");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("cant devide by zero ");
+            }
+        }
+    }
+}
